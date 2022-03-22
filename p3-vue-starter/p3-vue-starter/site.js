b62f71f10ba0550b74e3203043eb29464e979154
aaron Tolete <222atolete@frhsd.com>
2022-03-22 10:00:13 -0400

    update

--- p3-vue-starter/p3-vue-starter/site.js
+++ p3-vue-starter/p3-vue-starter/site.js
@@ -20,13 +20,8 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-<title>"IMDB + Aaron's Top 8 Movies"</title>
 
-
-
-
-const vue_app = new Vue({
-      el: '#vue_app',
+const vue_app = Vue.createapp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
       created () {
@@ -34,16 +29,80 @@ const vue_app = new Vue({
                   this.movies = json
             })
       },
-      data: {
+      data() {
+        return {
             // This holds your movies.json data.
-            movies: [Shark Tale, Goodfellas, ],
+            movies: [],
 
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: "IMDB + Aaron's Top 8 Movies",
+            title: "IMBD + Aaron Tolete's Top 8 Movies",
             owner: "Aaron Tolete",
-            github: ''
+            github: 'https://github.com/m-herbert?tab=repositories'
+          }
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray)
+            {
+            let month = ""
+              switch (dateArray[1]){
+                case 1:
+                 month = "January";
+                 break;
+                case 2:
+                  month = "February";
+                  break;
+                case 3:
+                  month = "March";
+                  break;
+                case 4:
+                  month = "April";
+                  break;
+                case 5:
+                  month = "May";
+                  break;
+                case 6:
+                  month = "June";
+                  break;
+                case 7:
+                  month = "July";
+                  break;
+                case 8:
+                  month = "August";
+                  break;
+                case 9:
+                  month = "September";
+                  break;
+                case 10:
+                  month= "October";
+                  break;
+                case 11:
+                  month = "November";
+                  break;
+                case 12:
+                  month = "December";
+                  break;
+                default:
+                  month = "Month";
+              }
+              return month + " " + dateArray[2] + ", " + dateArray[0];
+            },
+
+            posterClick(index)
+            {
+              if(this.movies[index].posterindex < this.movies[index].poster.length - 1)
+              {
+                this.movies[index].posterindex++;   }
+                else {
+                  this.movies[index].posterindex = 0;
+              }
+            },
+
+            textTime(minutes)
+            {
+              return Math.trunc(minutes/60) + "h " + (minutes%60) + "m";
+            }
       }
 })
+
+vue_app.mount("#vue_app")
